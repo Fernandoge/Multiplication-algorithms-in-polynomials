8625375d3df8d89a5c1ac89bf83c3ecb6e4e632a
Fernando <fernando.gp101@gmail.com>
2018-11-25 22:30:33 -0300

    Reduce and Conquer multiplication

--- lab1.c
+++ lab1.c
@@ -21,8 +21,13 @@ typedef struct Polinomio {
 	
 void showPoli(Poly X, char name);
 Poly createPoly(unsigned long grado, int random);
-Poly copyPoli(Poly X, unsigned long grado);
-
+Poly copyPoli(Poly X, int signo, unsigned long grado);
+Poly sumaPoli(Poly X, Poly Y);
+Poly restaPoli(Poly X, Poly Y);
+Poly mulClas(Poly X, Poly Y);
+Poly mulClasRyD(Poly X, Poly Y);
+Poly polyByConstant(Poly X, unsigned long a);
+Poly polyByX(Poly X, unsigned long a);
 
 /*---------------------------------------------------------------------------------------------------------------------------------------*/
 
@@ -70,10 +75,53 @@ int main(){
 	printf("\n-------------------------------------------------\n");
 	printf("Polinomios copiados:");
 	printf("\n-------------------------------------------------\n");		
-	C = copyPoli(A, A.g);
+	C = copyPoli(A, 0,A.g);
 	showPoli(C, 'C');
-	D = copyPoli(B, B.g);
+	D = copyPoli(B, 0,B.g);
 	showPoli(D, 'D');
+
+	printf("\n-------------------------------------------------\n");	
+	printf("Suma de polinomios C(x)=A(x)+B(x)");
+	printf("\n-------------------------------------------------\n");	
+	begin = clock();
+	showPoli(sumaPoli(A, B), 'C');
+	end = clock();
+	time_spent = (double)(end - begin)/CLOCKS_PER_SEC; 
+	printf("Tiempo empleado:\t%f\n", time_spent);
+	
+	printf("\n-------------------------------------------------\n");	
+	
+	printf("Resta de polinomios C(x)=A(x)-B(x)");
+	printf("\n-------------------------------------------------\n");	
+	begin = clock();
+	showPoli(restaPoli(A, B), 'C');
+	end = clock();
+	time_spent = (double)(end - begin)/CLOCKS_PER_SEC; 
+	printf("Tiempo empleado:\t%f\n", time_spent);
+	
+	printf("\n-------------------------------------------------\n");	
+	printf("Multiplicacion Clasica C(x)=A(x)xB(x)");
+	printf("\n-------------------------------------------------\n");	
+	begin = clock();
+	showPoli(mulClas(A, B), 'C');
+	end = clock();
+	time_spent = (double)(end - begin)/CLOCKS_PER_SEC; //Diferencia y transformacion a segundos
+	printf("Tiempo empleado:\t%f\n", time_spent);
+	
+	printf("\n-------------------------------------------------\n");	
+	
+	printf("Multiplicacion Clasica Reducir y Conquistar C(x)=A(x)xB(x)");
+	printf("\n-------------------------------------------------\n");	
+	if(A.g == B.g) {
+		begin = clock();
+		showPoli(mulClasRyD(A, B), 'C');
+		end = clock();
+		time_spent = (double)(end - begin)/CLOCKS_PER_SEC; //Diferencia y transformacion a segundos
+	printf("Tiempo empleado:\t%f\n", time_spent);
+	} else
+		printf("Polinomios deben ser de igual grado!");	
+
+	
 	free(A.p);
 	free(B.p);
 	free(C.p);
@@ -92,7 +140,7 @@ void showPoli(Poly X, char name) {
 	else
 		printf("0"); 	
 		
-	for(i=1; i<(X.g)+1; i++) //desde el segundo termino agrega "+" cuando el coeficiente de x^i es positivo
+	for(i=1; i<(X.g)+1; i++) 
 		if(X.p[i]<0)
 			printf(" %ld(X^%lu)", X.p[i], i); 
 		else
@@ -141,7 +189,7 @@ Poly createPoly(unsigned long grado, int random) { //random=1 crear coeficientes
 
 /*---------------------------------------------------------------------------------------------------------------------------------------*/
 
-Poly copyPoli(Poly X, unsigned long grado) { 
+Poly copyPoli(Poly X, int signo, unsigned long grado) {
 	Poly Z;												
 	unsigned long i;
 
@@ -154,10 +202,12 @@ Poly copyPoli(Poly X, unsigned long grado) {
 			exit(0);
 		}
 		
-		for(i=0; i<(X.g)+1; i++) 
-			Z.p[i]=X.p[i];
-		
-
+		if(!signo)
+			for(i=0; i<(X.g)+1; i++) 
+				Z.p[i]=X.p[i];
+		else
+			for(i=0; i<(X.g)+1; i++) 
+				Z.p[i]=(-1)*X.p[i];
 		
 		return Z;
 	} else	if (grado < X.g) {
@@ -169,9 +219,12 @@ Poly copyPoli(Poly X, unsigned long grado) {
 					exit(0);
 				}
 
-			
-				for(i=0; i<(grado)+1; i++) 
-					Z.p[i]=X.p[i];
+				if(!signo)
+					for(i=0; i<(grado)+1; i++)
+						Z.p[i]=X.p[i];
+				else
+					for(i=0; i<(grado)+1; i++) 
+						Z.p[i]=(-1)*X.p[i];
 
 				return Z;		
 
@@ -180,5 +233,165 @@ Poly copyPoli(Poly X, unsigned long grado) {
 				exit(0);
 				}
 
+	
+}
+
+/*---------------------------------------------------------------------------------------------------------------------------------------*/
+
+Poly sumaPoli(Poly X, Poly Y){ 
+	unsigned long i=0, max, sizeX = (X.g)+1, sizeY = (Y.g)+1;
+	Poly Z;	
+	
+	max = sizeX <= sizeY ? sizeY : sizeX;	
+	Z.p = (long *)calloc(max, sizeof(long));
+	Z.g = max-1;
+	
+	if(Z.p == NULL){
+		printf("Error, memoria no reservada!\n");
+		exit(0);
+	}		
+
+	if(sizeX <= sizeY){
+		Z = copyPoli(Y, 0, Y.g);
+		while(i < sizeX){
+		Z.p[i] = X.p[i]+Z.p[i];
+		i++;
+		}		
+	}	
+	else{
+		Z = copyPoli(X, 0, X.g);
+		while(i < sizeY){
+		Z.p[i] = Z.p[i]+Y.p[i];
+		i++;
+		}
+	}
+
+	return Z;
+}
+
+/*---------------------------------------------------------------------------------------------------------------------------------------*/
+
+Poly restaPoli(Poly X, Poly Y){ 
+	unsigned long i=0, max, sizeX = (X.g)+1, sizeY = (Y.g)+1;
+	Poly Z;
+	
+	max = sizeX <= sizeY ? sizeY : sizeX;	
+	Z.p = (long *)calloc(max, sizeof(long));
+	Z.g = max-1;
+	
+	if(Z.p == NULL){
+		printf("Error, memoria no reservada!\n");
+		exit(0);
+	}	
+	
+	if(sizeX <= sizeY){
+		Z = copyPoli(Y, 1, Y.g);
+		while(i < sizeX){
+		Z.p[i] = X.p[i]+Z.p[i];
+		i++;
+		}
+	}	
+	else{
+		Z = copyPoli(X, 0, X.g);
+		while(i < sizeY){
+		Z.p[i] = Z.p[i]-Y.p[i];
+		i++;
+		}
+	}
+
+	return Z;
+}
+
+/*---------------------------------------------------------------------------------------------------------------------------------------*/
+
+Poly polyByConstant(Poly X, unsigned long a) { 
+	unsigned long i;
+	Poly Z;
+
+	Z.g = X.g;
+	Z.p = (long *)calloc( (X.g)+1, sizeof(long));	
+	
+	if(Z.p == NULL){
+		printf("Error, memoria no reservada!\n");
+		exit(0);
+	}
+
+	for(i=0; i<(X.g)+1; i++) {
+		Z.p[i] = a*(X.p[i]); 
+	}
+
+	return Z;
+}
+
+/*---------------------------------------------------------------------------------------------------------------------------------------*/
+
+Poly polyByX(Poly X, unsigned long a) { 
+	unsigned long i;
+	Poly Z;
+
+	Z.g = (X.g)+a;
+	Z.p = (long *)calloc( (Z.g)+1, sizeof(long));	
+	
+	if(Z.p == NULL){
+		printf("Error, memoria no reservada!\n");
+		exit(0);
+	}
+
+	for(i=0; i<(X.g)+1; i++) { 
+		Z.p[i+a] = X.p[i]; 
+	}
+
+	return Z;
+}
+
+/*---------------------------------------------------------------------------------------------------------------------------------------*/
+Poly mulClas(Poly X, Poly Y){ 
+	unsigned long i, j, sizeX = (X.g)+1, sizeY = (Y.g)+1;
+	Poly Z;
+
+	Z.g = X.g + Y.g;
+	Z.p = (long *)calloc( (Z.g)+1, sizeof(long));
+	
+	
+	if(Z.p == NULL){
+		printf("Error, memoria no reservada!\n");
+		exit(0);
+	}	
+
+	for(i=0; i<sizeX; i++){
+		for(j=0; j<sizeY; j++){
+			Z.p[i+j] += X.p[i]*Y.p[j];
+		}
+	}
+
+	return Z;
+}
+
+/*---------------------------------------------------------------------------------------------------------------------------------------*/
+
+Poly mulClasRyD(Poly X, Poly Y) { 
+	Poly Z, A, B;
+
+	Z.g = X.g + Y.g;
+	Z.p = (long *)calloc( (Z.g)+1, sizeof(long));
+
+	if(Z.p == NULL){
+		printf("Error, memoria no reservada!\n");
+		exit(0);
+	}
+
+	if(Z.g == 0) {
+		Z.p[0]=0;
+		return Z;
+	} else {
+		A = copyPoli(X, 0, (X.g)-1); 
+		B = copyPoli(Y, 0, (Y.g)-1);
+
+		Z.p[(Z.g)] = (X.p[X.g])*(Y.p[Y.g]); 
+		Z = sumaPoli( Z, polyByX( sumaPoli( polyByConstant(B, X.p[X.g]), polyByConstant(A, Y.p[Y.g]) ), X.g ) ); 
+		Z = sumaPoli(Z, mulClas(A, B) );
+
+	return Z;
+	}
 	
 }
